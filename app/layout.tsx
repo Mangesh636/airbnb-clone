44433dc3f64f58ef3b1917e3e78185f368e68f1c
Mangesh636 <104110612+Mangesh636@users.noreply.github.com>
2024-01-26 00:40:26 +0530

    fetching currentuser

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import Navbar from "@/components/navbar/Navbar";
 import LoginModal from "@/components/modals/LoginModal";
 import RegisterModal from "@/components/modals/RegisterModal";
 import ToasterProvider from "@/providers/ToasterProvider";
+import getCurrentUser from "@/actions/getCurrentUser";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -16,18 +17,20 @@ export const metadata: Metadata = {
   description: "Airbnb Clone",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={nunito.className}>
         <ToasterProvider />
         <LoginModal />
         <RegisterModal />
-        <Navbar />
+        <Navbar currentUser={currentUser} />
         <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
